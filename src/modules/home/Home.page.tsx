b9a34dd6dae54b4d38b85f591448d166828667f8
Ailin Miño <pukyailin@gmail.com>
2025-07-04 16:04:07 -0300

    footer

--- src/modules/home/Home.page.tsx
+++ src/modules/home/Home.page.tsx
@@ -1,15 +1,20 @@
 import { useAuth } from "../../context/AuthContext";
 import Navbar from "../../common/components/navbar";
+import Footer from "../../common/components/Footer";
 
 const HomePage = () => {
   const { user } = useAuth();
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-fund text-center">
+    <>
       <Navbar />
-      <h1 className="text-3xl font-bold mb-4">¡Bienvenido a LeDi!</h1>
-      <p className="text-lg mb-6">ID de usuario: {user?.id}</p>
-    </div>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-fund text-center">
+        <h1 className="text-3xl font-bold mb-4">¡Bienvenido a LeDi!</h1>
+        <p>Hola Mundo, ¡Bienvenido a LeDi!</p>
+        <p className="text-lg mb-6">ID de usuario: {user?.id}</p>
+      </div>
+      <Footer />
+    </>
   );
 };
 
