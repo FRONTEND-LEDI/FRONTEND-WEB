76bf5842244bcc9c1caa43b4ed4d1a3ca935328c
fleita selene <fleitaselene26@gmail.com>
2025-08-29 16:25:25 -0300

    Cursor-Pointer incorpored

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import AppRouter from "./router";
 import { Toaster } from "react-hot-toast";
-import Navbar from './common/components/navbar';
+
 
 function App() {
   return (
     <>
       <Toaster position="top-right" reverseOrder={false} />
       <AppRouter />
-      <Navbar/>
+  
       
     </>
   );
